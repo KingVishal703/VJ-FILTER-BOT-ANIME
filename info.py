e2d1f5b7131d1f182313f9b86bd10e06eac4c62b
KingVishal703 <vishal21wankhede@gmail.com>
2024-11-15 13:02:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,8 +106,8 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Movies_King_Updat
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', '6DRRpJbz96ghKtCBgiORSGMnJ6H3')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'omegalinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '0178e6bc620a16f5ffe5e210731a2c8a375b102f')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
